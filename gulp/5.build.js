53074ab1590a1f295ea39032ec0f31366a55b9f9
Daniel <d.groen@codefield.nl>
2025-04-04 21:58:48 +0200

    up

--- gulp/5.build.js
+++ gulp/5.build.js
@@ -1,20 +1,26 @@
-const   gulp = require('gulp'),
-        del = require('del');
+const gulp = require("gulp"),
+  del = require("del");
 
-gulp.task('build:remove', () => {
-    return del([`${global.paths.dist}`]);
+gulp.task("build:remove", () => {
+  return del([`${global.paths.dist}`]);
 });
 
-gulp.task('build:build', () => {
-  return gulp.src(`${global.paths.app}/**/*`)
-             .pipe(gulp.dest(`./dist`));
+gulp.task("build:build", () => {
+  return gulp.src(`${global.paths.app}/**/*`).pipe(gulp.dest(`./docs`));
 });
 
-gulp.task('build:clean', () => {
-  return del([
-              `${global.paths.dist}/sass`,
-            ]);
+gulp.task("build:clean", () => {
+  return del([`${global.paths.dist}/sass`]);
 });
 
-
-gulp.task('build', gulp.series('sass','fonts', 'build:remove','build:build', 'build:clean', 'minify'));
+gulp.task(
+  "build",
+  gulp.series(
+    "sass",
+    "fonts",
+    "build:remove",
+    "build:build",
+    "build:clean",
+    "minify"
+  )
+);
